11bf3a99b5637eee4184f578af2074f235c9214e
hnlyzxf <672149402@qq.com>
2023-07-25 02:13:41 +0800

    Site updated: 2023-07-25 02:13:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,5 +1,20 @@
 var posts=["2023/07/21/Bandizip7 Windows版破解补丁[支持版本更新]/","2023/06/28/hello-world/","2023/06/29/GitHub部署Hexo/","2023/07/02/Markdown常用语法汇总/","2023/06/30/解决github部署完成需重绑域名/","2023/07/03/office2016安装教程/","2023/07/23/解决cloudflare526报错/","2023/07/01/黄河科技学院校历、地图等/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};var friend_link_list=[{"name":"Hexo","hundredSuffix":"","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/15/63232b7d91d22.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-blog@1.1.6/img/post/common/anzhiy.cn.jpg"},{"name":"峰见","hundredSuffix":"","link":"https://i.x0h.cn/","avatar":"https://raw.githubusercontent.com/hnlyzxf/image/main/blog/touxiang.jpg","descr":"在平凡的日子里热爱生活","siteshot":"https://fastly.jsdelivr.net/gh/hnlyzxf/image/blog/202307081040790.png"},{"name":"峰见","hundredSuffix":"","link":"https://i.x0h.cn","avatar":"https://raw.githubusercontent.com/hnlyzxf/image/main/blog/touxiang.jpg","descr":"在平凡的日子里热爱生活"}];
     var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
     function addFriendLinksInFooter() {
       var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
       if(!footerRandomFriendsBtn) return;
